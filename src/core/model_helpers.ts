603f47bba43d3da30d81ab458b40f57fd99bb351
Jason Reed <jcreed@gmail.com>
2023-10-05 18:17:21 -0400

    Generate random letters

--- src/core/model_helpers.ts
+++ src/core/model_helpers.ts
@@ -11,6 +11,10 @@ export function is_occupied(state: GameState, p: Point): boolean {
 export function peelOfState(state: GameState): GameState {
   const p_in_world_int = vm(apply(inverse(state.canvas_from_world), state.mouseState.p), Math.floor);
   return produce(state, s => {
-    s.tiles.push({ letter: 'Q', p_in_world_int });
+    s.tiles.push({ letter: randomLetter(), p_in_world_int });
   });
+}
+
+export function randomLetter(): string {
+  return 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'[Math.floor(Math.random() * 26)];
 }
