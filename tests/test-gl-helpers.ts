ec29fccccfd6431039b0bb59c278187c73b30d44
Jason Reed <jcreed@gmail.com>
2024-02-05 18:36:50 -0500

    Fix broken tests

--- tests/test-gl-helpers.ts
+++ tests/test-gl-helpers.ts
@@ -6,10 +6,10 @@ describe('canvas_from_gl', () => {
   test('should map points as expected', () => {
 
     expect(apply(gl_from_canvas, { x: 0, y: 0 })).toEqual({ x: -1, y: 1 });
-    expect(apply(gl_from_canvas, { x: 800, y: 0 })).toEqual({ x: 1, y: 1 });
+    expect(apply(gl_from_canvas, { x: 1024, y: 0 })).toEqual({ x: 1, y: 1 });
 
-    expect(apply(gl_from_canvas, { x: 0, y: 600 })).toEqual({ x: -1, y: -1 });
-    expect(apply(gl_from_canvas, { x: 800, y: 600 })).toEqual({ x: 1, y: -1 });
+    expect(apply(gl_from_canvas, { x: 0, y: 768 })).toEqual({ x: -1, y: -1 });
+    expect(apply(gl_from_canvas, { x: 1024, y: 768 })).toEqual({ x: 1, y: -1 });
 
   });
 
--- tests/test-tile-helpers.ts
+++ tests/test-tile-helpers.ts
@@ -7,11 +7,11 @@ const SEED = 12345678;
 
 function testState(): GameState {
   let state = mkGameState(SEED, false, SEED);
-  state = produce(state, s => addWorldTile(s.coreState, { letter: 'A', p_in_world_int: { x: 0, y: 0 }, id: '1' }));
-  state = produce(state, s => addWorldTile(s.coreState, { letter: 'B', p_in_world_int: { x: 1, y: 0 }, id: '2' }));
-  state = produce(state, s => addHandTileEntity(s.coreState, 'C', 0, '3'));
-  state = produce(state, s => addHandTileEntity(s.coreState, 'D', 1, '4'));
-  state = produce(state, s => addHandTileEntity(s.coreState, 'E', 2, '5'));
+  state = produce(state, s => { addWorldTile(s.coreState, { letter: 'A', p_in_world_int: { x: 0, y: 0 }, id: '1' }); });
+  state = produce(state, s => { addWorldTile(s.coreState, { letter: 'B', p_in_world_int: { x: 1, y: 0 }, id: '2' }); });
+  state = produce(state, s => { addHandTileEntity(s.coreState, 'C', 0, '3'); });
+  state = produce(state, s => { addHandTileEntity(s.coreState, 'D', 1, '4'); });
+  state = produce(state, s => { addHandTileEntity(s.coreState, 'E', 2, '5'); });
   return state;
 }
 
@@ -19,8 +19,8 @@ describe('tile operations', () => {
   test('should do putInWorld correctly', () => {
     const state0 = testState().coreState;
     const state1 = putTileInWorld(state0, '4', { x: 2, y: 0 });
-    expect(getTileLoc(state1, '3')).toEqual({ t: 'hand', p_in_hand_int: { x: 0, y: 0 } });
-    expect(getTileLoc(state1, '5')).toEqual({ t: 'hand', p_in_hand_int: { x: 0, y: 1 } });
+    expect(getTileLoc(state1, '3')).toEqual({ t: 'hand', index: 0 });
+    expect(getTileLoc(state1, '5')).toEqual({ t: 'hand', index: 1 });
   });
 
 });
