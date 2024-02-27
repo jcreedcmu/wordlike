7670a3c1d4d038b81cab1d39e4597d03d30da873
Jason Reed <jcreed@gmail.com>
2024-02-27 12:35:59 -0500

    Clean up example state generation
    This should save me from a bunch of boring changes in the future

--- src/core/exampleState.ts
+++ src/core/exampleState.ts
@@ -1,112 +1,34 @@
 import { PANIC_INTERVAL_MS } from '../core/clock';
-import { mkGridOf } from '../core/grid';
 import { ensureTileId } from "../core/id-helpers";
-import { mkOverlay } from '../core/layer';
 import { AbstractLetter } from '../core/letters';
-import { mkMobsState } from '../core/mobs';
-import { GameState, TileOptionalId } from '../core/state';
+import { GameState, TileOptionalId, mkGameState } from '../core/state';
 import { addHandTileEntities, addWorldTiles, checkValid, resolveValid, withCoreState } from '../core/state-helpers';
 import { produce } from '../util/produce';
 import * as SE1 from '../util/se1';
 import { updateFogOfWar } from './fog-of-war';
 
 export function exampleState(): GameState {
-  const state: GameState = {
-    coreState: {
-      slowState: {
-        generation: 0,
-        inventory: {
-          dynamites: 5,
-          bombs: 3,
-          vowels: 0,
-          consonants: 0,
-          copies: 0,
-          times: 0,
-          glasses: 0,
-        },
-        resource: {
-          wood: 0,
-          stone: 0,
-        },
-        scoring: {
-          score: 7,
-          highWaterMark: 7,
-        },
-        currentTool: 'pointer',
-        invalidWords: [],
-        renderToGl: true,
-        paused: undefined,
-        winState: { t: 'playing' },
-      },
-      wordBonusState: {
-        active: [],
-        numAllocated: 0,
-      },
-      _cacheUpdateQueue: [],
-      animations: [],
-      mobile_entities: {},
-      seen_cells: mkOverlay(),
-      connectedSet: mkGridOf([]),
-      energies: {
-        byLetter: [
-          -0.20972339977922094,
-          0.7840271889992784,
-          1.3068528194400546,
-          0.3068528194400547,
-          0.5636791674230779,
-          1.6534264097200273,
-          0.3068528194400547,
-          0.7840271889992784,
-          0.6791654891096679,
-          4,
-          4,
-          1.1041202653859723,
-          0.7840271889992784,
-          0.4602792291600821,
-          0.4602792291600821,
-          0.7840271889992784,
-          4,
-          0.4602792291600821,
-          -0.0047189562170500965,
-          -0.039720770839917874,
-          0.79528104378295,
-          1.6534264097200273,
-          1.6534264097200273,
-          4,
-          1.6534264097200273,
-          0
-        ],
-        byClass: [0, 0,],
-      },
-      seed: 1533311107,
-      bonusLayerSeed: 46,
-      canvas_from_world: {
-        scale: {
-          x: 39.6694214876033,
-          y: 39.6694214876033
-        },
-        translate: {
-          x: 150.80991735537191,
-          y: 215.56198347107437
-        }
-      },
-      game_from_clock: SE1.ident(),
-      bonusOverlay: {
-        cells: {
+  const state = mkGameState(1533311107, false, 46);
 
-        }
-      },
-      panic: { currentTime_in_game: Date.now(), lastClear_in_game: Date.now() - PANIC_INTERVAL_MS / 3 },
-      mobsState: mkMobsState(),
-    },
-    mouseState: {
-      t: "up",
-      p_in_canvas: {
-        x: 962,
-        y: 88
-      }
-    },
+  state.coreState.slowState.inventory.bombs = 3;
+  state.coreState.slowState.scoring = {
+    score: 7,
+    highWaterMark: 7,
   };
+
+  state.coreState.canvas_from_world = {
+    scale: {
+      x: 39.6694214876033,
+      y: 39.6694214876033
+    },
+    translate: {
+      x: 150.80991735537191,
+      y: 215.56198347107437
+    }
+  };
+
+  state.coreState.panic = { currentTime_in_game: Date.now(), lastClear_in_game: Date.now() - PANIC_INTERVAL_MS / 3 };
+  state.coreState.game_from_clock = SE1.ident();
 
   const tois: TileOptionalId[] = [
     { letter: { t: 'single', letter: "p" }, p_in_world_int: { x: 0, y: 0 } },
