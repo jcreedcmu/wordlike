c10d61c23c16b6428b114c035c0e2fb0ceeadeb6
Jason Reed <jcreed@gmail.com>
2024-02-28 15:58:41 -0500

    Don't feed id to mobsMapVal callback

--- src/core/mobs-map.ts
+++ src/core/mobs-map.ts
@@ -7,6 +7,6 @@ export function mobsMap<T>(state: MobsState, f: (mob: MobState, id: MobileId) =>
   return Object.keys(state.mobs).map(key => f(state.mobs[key], state.mobs[key].id));
 }
 
-export function mobsMapVal(state: MobsState, k: (mob: MobState, id: string) => MobState): MobsState {
+export function mobsMapVal(state: MobsState, k: (mob: MobState) => MobState): MobsState {
   return { mobs: mapval(state.mobs, k) };
 }
